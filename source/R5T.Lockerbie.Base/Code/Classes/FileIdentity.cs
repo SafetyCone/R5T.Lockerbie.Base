480f9c31fb8f9c58da6473f58f3ab8310d5d18f9
MinexAutomation <minex.automation@gmail.com>
2019-11-11 00:36:43 -0800

    Added static New() method.

--- source/R5T.Lockerbie.Base/Code/Classes/FileIdentity.cs
+++ source/R5T.Lockerbie.Base/Code/Classes/FileIdentity.cs
@@ -7,6 +7,17 @@ namespace R5T.Lockerbie
 {
     public class FileIdentity : TypedGuid
     {
+        #region Static
+
+        public static FileIdentity New(Guid value)
+        {
+            var output = new FileIdentity(value);
+            return output;
+        }
+
+        #endregion
+
+
         public FileIdentity(Guid value)
             : base(value)
         {
